1926194f8450abf1a7a4e3298ef1986a6a01aba6
yajieniu <yajieniu@wireless-10-147-166-83.public.utexas.edu>
2019-03-16 16:52:13 -0500

    framework

---
+++ socialRobot.py
@@ -0,0 +1,50 @@
+from Map import *
+from Agent import *
+
+class Game:
+	def __init__(self):
+		self.map = Map()
+		self.priority = 1
+		self.robot = Robot(self.priority)
+		self.map.create_room()
+		self.map.create_robot()
+		self.reward_table = [self.map.width][self.map.height]
+
+	def update(self):
+
+		r = getReward(self.robot.state)
+
+		# get human feedback
+
+		e, h = getHumanFeedback()
+
+		r = updateR(e, h, r)
+
+		WriteRToFile()
+
+		ChooseAction()
+
+		UpdateMap()
+
+		self.robot.state = UpdateState()
+			
+
+	def startNewGame(self):
+		clearEverything()
+		reInit()
+
+
+while True:
+	game.update()
+	if game.endGame:
+		ans = raw_input("New Game?")
+
+		if ans=='y':
+			startNewGame()
+		else:
+			finalReport()
+			break
+
+
+
+
